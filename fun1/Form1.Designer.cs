90376bcd69f3ad79f323032757ed249e10f5ee0b
Anton Kotlyar <kotlaranton551@gmail.com>
2023-06-20 20:49:37 +0300

    Добавьте файлы проекта.

---
+++ fun1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace fun1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 167);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(719, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 50F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 89);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R:    G:     B:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 8F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(579, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Чтобы скопировать\r\nцвет, нажмите CTRL + C\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(701, 86);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ЦВЕТ СКОПИРОВАН";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(744, 279);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ fun1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fun1
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        Thread th;
+        bool stopThread = false;
+
+        public Form1()
+        {
+            InitializeComponent();      
+            th = new Thread(SetCursorPositionColor);
+            th.Start();
+
+            panel1.BackColor = Color.FromArgb(200, panel1.BackColor);
+        }
+
+        private void SetCursorPositionColor()
+        {
+            while (!stopThread)
+            { 
+                Color color = ScreenColorPicker.GetCursorPositionColor();
+                BackColor = color;
+                label1.ForeColor = color;
+                label2.ForeColor = color;
+                label1.Text = $"R:{color.R}\t G:{color.G}\t B:{color.B}";
+                //Thread.Sleep(100);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stopThread = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    public class ScreenColorPicker
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern uint GetPixel(IntPtr hdc, int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+
+        public static Color GetCursorPositionColor()
+        {
+            Point cursorPosition = Cursor.Position;
+            IntPtr desktopWindowHandle = IntPtr.Zero;
+            IntPtr desktopWindowDC = IntPtr.Zero;
+
+            try
+            {
+                desktopWindowHandle = GetDesktopWindow();
+                desktopWindowDC = GetDC(desktopWindowHandle);
+
+                uint pixelColor = GetPixel(desktopWindowDC, cursorPosition.X, cursorPosition.Y);
+                Color color = Color.FromArgb((int)(pixelColor & 0x000000FF), (int)(pixelColor & 0x0000FF00) >> 8, (int)(pixelColor & 0x00FF0000) >> 16);
+
+                return color;
+            }
+            finally
+            {
+                if (desktopWindowDC != IntPtr.Zero)
+                    ReleaseDC(desktopWindowHandle, desktopWindowDC);
+            }
+        }
+    }
+}
